c3e71f85c05543865c602771529f6c8726def904
mneller <martin@ellermeier.net>
2020-12-13 10:20:27 +0100

    Initial commit

---
+++ src/main/java/net/ellermeier/aircraftdemo/AircraftdemoApplication.java
@@ -0,0 +1,13 @@
+package net.ellermeier.aircraftdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AircraftdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ellermeier/aircraftdemo/AircraftdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ellermeier.aircraftdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
